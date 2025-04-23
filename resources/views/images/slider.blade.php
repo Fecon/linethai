d429b3d2427c57ecb60a385ccb974b82717733bf
Fecon <l2uncs@gmail.com>
2025-04-23 17:01:59 +0700

    update swiper

--- resources/views/images/slider.blade.php
+++ resources/views/images/slider.blade.php
@@ -3,12 +3,11 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Swiper demo</title>
+    <title>LINE THAI - Sak Yant Tattoo Studio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
     <!-- Link Swiper's CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="{{ asset('swiper/swiper-bundle.min.css') }}">
 
-    <!-- Demo styles -->
     <style>
         html,
         body {
@@ -61,7 +60,7 @@
     </div>
 
     <!-- Swiper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="{{ asset('swiper/swiper-bundle.min.js') }}"></script>
 
     <!-- Initialize Swiper -->
     <script>
